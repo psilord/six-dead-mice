327fddd807c76db68142d96f6d2f557f471e22cc
Peter Keller <psilord@cs.wisc.edu>
2015-08-03 01:23:33 -0500

    Tinkered with a docstring.

--- lisp-object.lisp
+++ lisp-object.lisp
@@ -233,7 +233,9 @@ storable in a PLACE."
         *index* 0))
 
 (defun //alloc-mem (num-cells)
-  "Allocate NUM-CELLs of memory, then return the address of the start of it."
+  "Allocate NUM-CELLs of memory, then return the address of the start
+of it. The allocation is a simple increment of a pointer through
+memory."
   (let ((mem *index*))
     (incf *index* num-cells)
     mem))
